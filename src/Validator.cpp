c86575c6362a511ac2f09e02a5aa3461fad6bcf7
Dacia Rios <71524653+DaciaRios@users.noreply.github.com>
2024-05-18 09:07:50 -0700

    Update Validator.cpp

--- src/Validator.cpp
+++ src/Validator.cpp
@@ -1,1 +1,8 @@
 #include "../headers/Validator.h"
+#include <cstdlib> //for the rand()
+
+float Validator::evaluationFunction(){ //i'm assuming this will eventually take in a vector of set??
+      //stub function 
+      return  static_cast <float> (rand()) / ( static_cast <float> (RAND_MAX/(100-0))); //return random number betwwen 0 and 100
+}
+  
