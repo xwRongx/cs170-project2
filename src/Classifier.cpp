44ddea79f736898dbccfdd9dde41b76ba4114527
Citrushappy <36286623+CitrusHappy@users.noreply.github.com>
2024-05-24 18:29:18 -0700

    wow

--- src/Classifier.cpp
+++ src/Classifier.cpp
@@ -13,14 +13,14 @@ void Classifier::train(map<Instance, float>* incomingDataSet){ //kind of a sette
 //NN classifier
 float Classifier::test(Instance* testInstance){
     //compute distance of testInstance to all training points (in dataset)
-    auto* closestInstance = new Instance();
+    Instance closestInstance = new Instance();
     float closestInstanceSimilarity; //stores similarity value (calculated with euclidean) of the closest training instance
 
-    for(auto data : *dataset){
-        float euclideanDist = euclideanDistance(data.first /*data's Instance*/, *testInstance);
+    for(auto instance : *dataset){
+        float euclideanDist = euclideanDistance(instance.first /*instance's Instance*/, *testInstance);
         if(closestInstanceSimilarity < euclideanDist){
             closestInstanceSimilarity = euclideanDist;
-            closestInstance = data.first;
+            closestInstance = instance.first;
         }
     }
     return closestInstance.classLabel; //return class label of the nearest training point
